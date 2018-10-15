ae5a3adf8b057c4032bef8f91e60b3cc19017083
Justin Chadwell <jedevc@gmail.com>
2018-10-15 15:22:09 +0100

    Add bash svg generation

--- build.sh
+++ build.sh
@@ -3,4 +3,25 @@
 BUILD=build/
 mkdir -p $BUILD/
 
-javac -d $BUILD source/Identicon.java
+ACTION=0
+
+if [[ source/Identicon.java -nt $BUILD/Identicon.class ]]; then
+	echo "Building java..."
+	ACTION=1
+
+	javac -d $BUILD source/Identicon.java
+fi
+
+if [[ source/Identicon.sh -nt $BUILD/Identicon.sh ]]; then
+	echo "Building bash..."
+	ACTION=1
+
+	cp source/Identicon.sh $BUILD/Identicon.sh
+	chmod u+x $BUILD/Identicon.sh
+fi
+
+if [[ $ACTION -eq 1 ]]; then
+	echo "Done."
+else
+	echo "Nothing to do."
+fi
---
+++ source/Identicon.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# canvas settings
+squares=5
+size=20
+canvas=$(( $squares * $size ))
+
+# color generation
+red=$(( RANDOM % 256 ))
+blue=$(( RANDOM % 256 ))
+green=$(( RANDOM % 256 ))
+color="rgb($red, $blue, $green)"
+
+# svg generation
+echo "<svg viewBox='0 0 $canvas $canvas' xmlns='http://www.w3.org/2000/svg'>"
+echo "<rect x='0' y='0' width='$canvas' height='$canvas' fill='#fff' />"
+for i in `seq 0 $(( $squares / 2 + 1))`; do
+    for j in `seq 0 $squares`; do
+        if [[ $(( RANDOM % 2 )) -eq 0 ]]; then
+            x=$(( $i * $size ))
+            altx=$(( ($squares - $i - 1) * $size ))
+            y=$(( $j * $size ))
+
+            echo "<rect x='$x' y='$y' width='$size' height='$size' fill='$color' />"
+            echo "<rect x='$altx' y='$y' width='$size' height='$size' fill='$color' />"
+        fi
+    done
+done
+echo "</svg>"
