cc9462b8980d7972c13a3edf1ec0cffe0d1e1c4c
Justin Chadwell <jedevc@gmail.com>
2018-11-16 11:23:24 +0000

    Refactoring shell code

--- source/shell/Identicon.sh
+++ source/shell/Identicon.sh
@@ -5,6 +5,8 @@ squares=5
 size=20
 canvas=$(( $squares * $size ))
 
+mirror=$(( $squares / 2 + 1 ))
+
 # random seeding
 if [[ -n $1 ]]; then
     RANDOM=$1
@@ -19,15 +21,17 @@ color="rgb($red, $green, $blue)"
 # svg generation
 echo "<svg viewBox='0 0 $canvas $canvas' xmlns='http://www.w3.org/2000/svg'>"
 echo "<rect x='0' y='0' width='$canvas' height='$canvas' fill='#fff' />"
-for i in `seq 0 $(( $squares / 2 + 1))`; do
-    for j in `seq 0 $squares`; do
+for i in `seq 0 $(( $mirror - 1 ))`; do
+    for j in `seq 0 $(( $squares - 1 ))`; do
         if [[ $(( RANDOM % 2 )) -eq 0 ]]; then
             x=$(( $i * $size ))
-            altx=$(( ($squares - $i - 1) * $size ))
             y=$(( $j * $size ))
 
             echo "<rect x='$x' y='$y' width='$size' height='$size' fill='$color' />"
-            echo "<rect x='$altx' y='$y' width='$size' height='$size' fill='$color' />"
+            if [[ $i -ne $(( $mirror - 1 )) ]]; then
+                altx=$(( ($squares - $i - 1) * $size ))
+                echo "<rect x='$altx' y='$y' width='$size' height='$size' fill='$color' />"
+            fi
         fi
     done
 done
